72bb17785fa4a60f0699d00ce63438900ec27d75
sangdo14 <sangdo14@naver.com>
2025-04-14 17:46:43 +0900

    feat: 사각형 회전 색상

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ const btnMove = document.querySelector('.btnMove');
 const btnReset = document.querySelector('.btnReset');
 
 btnMove.addEventListener('click', ()=>{
-  gsap.to('.box', {x: 300, y: 200, opacity:0, rotation: 360, duration: 1});
+  gsap.to('.box', {x: 300, y: 200, rotation: 360, duration: 1, backgroundColor: 'pink'});
 })
 
 btnReset.addEventListener('click', ()=>{
-  gsap.to('.box', {x: 0, y: 0, opacity:1, rotation: 0});
+  gsap.to('.box', {x: 0, y: 0, rotation: 0, backgroundColor: 'aqua'});
 })
