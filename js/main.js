8776c4f2138becf40b5d59b23981daec6cdbd67f
sangdo14 <sangdo14@naver.com>
2025-04-15 15:43:55 +0900

    feat: 마우스오버 깜빡이는 버튼

--- js/main.js
+++ js/main.js
@@ -3,21 +3,31 @@ const menu = document.querySelectorAll(".gnb li");
 const bgVideo = document.querySelector("video");
 const title = document.querySelector(".textBox h2");
 const btnOpen = document.querySelector(".btnOpen");
-
+let isOn = null;
 
 // scss에서 초기 위치 값을 수정하는 것이 아닌 set으로 초기위치값 변경
 gsap.set(logo, {y:-100, opacity:0})
 gsap.set(menu, {y:-100, opacity:0})
 gsap.set(bgVideo, {opacity:1})
 gsap.set(title, {y:-100, opacity:0})
-gsap.set(btnOpen, {scale:5, opacity:0} )
+gsap.set(btnOpen, {scale:3, opacity:0} )
 
 gsap.to(logo, {y:0, opacity:1, duration:1, delay:2})
-gsap.to(menu[0], {y:0, opacity:1, duration:1, delay:2})
-gsap.to(menu[1], {y:0, opacity:1, duration:1, delay:2.5})
-gsap.to(menu[2], {y:0, opacity:1, duration:1, delay:3})
-gsap.to(menu[3], {y:0, opacity:1, duration:1, delay:3.5})
-gsap.to(menu[4], {y:0, opacity:1, duration:1, delay:4.5})
+gsap.to(menu, {y:0, opacity:1, duration:1, delay:2, stagger:0.2})
+// gsap.to(menu[0], {y:0, opacity:1, duration:1, delay:2})
+// gsap.to(menu[1], {y:0, opacity:1, duration:1, delay:2.5})
+// gsap.to(menu[2], {y:0, opacity:1, duration:1, delay:3})
+// gsap.to(menu[3], {y:0, opacity:1, duration:1, delay:3.5})
+// gsap.to(menu[4], {y:0, opacity:1, duration:1, delay:4.5})
 gsap.to(bgVideo, {opacity:0.5, duration:5} )
 gsap.to(title, {y:0, opacity:1, duration:1, delay:1} )
 gsap.to(btnOpen, {scale:1, opacity:1, duration:1, delay:1.5} )
+
+btnOpen.addEventListener('mouseover',()=>{
+  console.log('mouseenter');
+  console.log(isOn);
+  isOn = gsap.to(btnOpen, {scale:1.2, repeat: -1, yoyo: true});
+});
+btnOpen.addEventListener('mouseleave',()=>{
+  isOn.kill();
+});
